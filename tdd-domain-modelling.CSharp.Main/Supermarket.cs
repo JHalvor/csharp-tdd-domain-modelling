1fde2433e615f30effefaa87b62b0c19e452505f
Jonas <jonashalvorsen2013@live.no>
2024-08-14 11:11:53 +0200

    Exercise-2-part1-complete

---
+++ tdd-domain-modelling.CSharp.Main/Supermarket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_domain_modelling.CSharp.Main
+{
+    public class Supermarket
+    {
+        private Dictionary<string, int> _basket = new Dictionary<string, int>();
+        public Dictionary<string, int> basket { get { return _basket; } }
+
+        public bool Add(string product, int price)
+        {
+            if (basket.ContainsKey(product))
+            {
+                return false;
+            }
+            basket.Add(product, price);
+            return true;
+        }
+    }
+}
---
+++ tdd-domain-modelling.CSharp.Test/SupermarketTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using tdd_domain_modelling.CSharp.Main;
+
+namespace tdd_domain_modelling.CSharp.Test
+{
+    [TestFixture]
+    public class SupermarketTest
+    {
+        [TestCase("Apple", 12, true)]
+        [TestCase("Banana", 5, true)]
+        [TestCase("Coffee", 8, true)]
+        public void TestAddNewProductInBasket(string product, int price, bool expectedBasketAddResult)
+        {
+            //Arrange
+            Supermarket customer = new Supermarket();
+
+            //Act
+            bool actualBasketAddResult = customer.Add(product, price);
+
+            //Assert
+            Assert.That(expectedBasketAddResult, Is.EqualTo(actualBasketAddResult));
+        }
+    }
+}
